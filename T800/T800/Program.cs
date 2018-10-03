c56799c90ce51e8e311bcf195d2c18cefd99d8c9
unknown <iperish.x@gmail.com>
2018-10-03 02:00:47 +0200

    Made more menu changes, added sliding drawer

--- T800/T800/Program.cs
+++ T800/T800/Program.cs
@@ -35,11 +35,12 @@ namespace T800
         //We first clear the console to get a base position of the cursor which then allows us to easily define specific
         //cells of the terminal program being used - this is USUALLY the very top left-most cell in the terminal. The menu
         //is then drawn based on that initial location. This location is our 0,0.
-    
-        public static int xCoord, yCoord;
 
-        static readonly int _length = 50;
-        static readonly int _height = 15;
+        public static int xCoord, yCoord, y;
+
+        public static bool DrawerOut = false;
+        static readonly int _length = 50; //These 2 values control the size of the box.
+        static readonly int _height = 15; //Changing these values resizes it respective of x and y
 
         public static String Username { get; set; }
         public static String Password { get; set; }
@@ -93,140 +94,140 @@ namespace T800
         {
             Username = username;
             DrawMenu(3);
-            choice = Console.ReadKey().Key;
-            switch (choice)
+            while (choice != ConsoleKey.Escape)
             {
-                case ConsoleKey.D1:
-
-                    break;
-                case ConsoleKey.D2:
-                    break;
-                case ConsoleKey.D3:
-                    break;
-                case ConsoleKey.D4:
-                    break;
-                case ConsoleKey.D5:
-                    break;
-                case ConsoleKey.D6:
-                    break;
-                case ConsoleKey.Escape:
-                    PreLoginMenu();
-                    break;
+                choice = Console.ReadKey().Key;
+                switch (choice)
+                {
+                    case ConsoleKey.D1:
+                        UnderBox();
+                        break;
+                    case ConsoleKey.D2:
+                        break;
+                    case ConsoleKey.D3:
+                        break;
+                    case ConsoleKey.D4:
+                        break;
+                    case ConsoleKey.D5:
+                        break;
+                    case ConsoleKey.D6:
+                        break;
+                    case ConsoleKey.Escape:
+                        PreLoginMenu();
+                        break;
+                }
             }
         }
 
         public static void DrawMenu(int menu)
         {
+            y = 0;
             switch(menu)
             {
                 case 1: //PRE-LOGIN
                     ClearInside();
-                    for (int x = 0; x < _length; x++) //TOP
+                    for (int x = 0; x < _length; x++) 
                     {
-                        WriteAt("#", xCoord + x, yCoord);
-                    }
-                    for (int x = 0; x < _height; x++) //LEFT
-                    {
-                        WriteAt("#", xCoord, yCoord + x);
-                    }
-                    for (int x = 0; x < _height; x++) //RIGHT
-                    {
-                        WriteAt("#", xCoord + _length, yCoord + x);
-                    }
-                    for (int x = 0; x <= _length; x++) //BOTTOM
-                    {
-                        WriteAt("#", xCoord + x, yCoord + _height);
+                        WriteAt("#", xCoord + x, yCoord); //TOP 
+                        Thread.Sleep(1);
+                        if (y <= _height) 
+                        {
+                            WriteAt("#", xCoord + _length, yCoord + x); //RIGHT
+                            WriteAt("#", xCoord, yCoord + x); //LEFT
+                            Thread.Sleep(1);
+                            y++;
+                        }
+                        WriteAt("#", xCoord + x, yCoord + _height); //BOTTOM
+                        Thread.Sleep(1);
                     }
                     for (int x = 0; x < _length; x++) //SEPARATOR AT LINE 6
                     {
                         WriteAt("#", xCoord + x, yCoord + 5);
+                        Thread.Sleep(1);
                     }
-                    WriteAt("TERMINATOR TERMINAL V1.0", 5, 2);
-                    WriteAt("You pick 'em, we kill'em!", 5, 3);
-                    WriteAt("PLEASE LOG IN TO CONTINUE", 5, 6);
+                    WriteAtJustified("TERMINATOR TERMINAL V1.0", 2);
+                    WriteAtJustified("You pick 'em, we kill'em!", 3);
+                    WriteAtJustified("PLEASE LOG IN TO CONTINUE", 6);
 
-                    WriteAt("1. LOG IN", 12, 10);
-                    WriteAt("ESC: EXIT", 12, 12);
+                    WriteAtJustified("1. LOG IN", 10);
+                    WriteAtJustified("ESC: EXIT", 12);
                     break;
 
                 case 2: //LOGIN
                     ClearInside();
-                    for (int x = 0; x < _length; x++) //TOP
+                    for (int x = 0; x < _length; x++)
                     {
-                        WriteAt("#", xCoord + x, yCoord);
-                    }
-                    for (int x = 0; x < _height; x++) //LEFT
-                    {
-                        WriteAt("#", xCoord, yCoord + x);
-                    }
-                    for (int x = 0; x < _height; x++) //RIGHT
-                    {
-                        WriteAt("#", xCoord + _length, yCoord + x);
-                    }
-                    for (int x = 0; x <= _length; x++) //BOTTOM
-                    {
-                        WriteAt("#", xCoord + x, yCoord + _height);
+                        WriteAt("#", xCoord + x, yCoord); //TOP 
+                        Thread.Sleep(1);
+                        if (y <= _height)
+                        {
+                            WriteAt("#", xCoord + _length, yCoord + x); //RIGHT
+                            WriteAt("#", xCoord, yCoord + x); //LEFT
+                            Thread.Sleep(1);
+                            y++;
+                        }
+                        WriteAt("#", xCoord + x, yCoord + _height); //BOTTOM
+                        Thread.Sleep(1);
                     }
                     for (int x = 0; x < _length; x++) //SEPARATOR AT LINE 6
                     {
                         WriteAt("#", xCoord + x, yCoord + 5);
+                        Thread.Sleep(1);
                     }
-                    WriteAt("TERMINATOR TERMINAL V1.0", 5, 2);
-                    WriteAt("You pick 'em, we kill'em!", 5, 3);
-                    WriteAt("PLEASE LOG IN TO CONTINUE", 5, 6);
+                    WriteAtJustified("TERMINATOR TERMINAL V1.0", 2);
+                    WriteAtJustified("You pick 'em, we kill'em!", 3);
+                    WriteAtJustified("PLEASE LOG IN TO CONTINUE", 6);
                     WriteAt("USERNAME: ", 4, 10);
                     WriteAt("PASSWORD: ", 4, 12);
-                    WriteAt("ESC: BACK", 15, 14);
+                    WriteAtJustified("ESC: BACK", 14); //This doesnt actually work, but maybe i should leave it in. Maybe teacher wont notice :thinking:
                     break;
 
                 case 3: //LOGGED IN
                     ClearInside();
-                    for (int x = 0; x < _length; x++) //TOP
+                    for (int x = 0; x < _length; x++)
                     {
-                        WriteAt("#", xCoord + x, yCoord);
-                    }
-                    for (int x = 0; x < _height; x++) //LEFT
-                    {
-                        WriteAt("#", xCoord, yCoord + x);
-                    }
-                    for (int x = 0; x < _height; x++) //RIGHT
-                    {
-                        WriteAt("#", xCoord + _length, yCoord + x);
-                    }
-                    for (int x = 0; x <= _length; x++) //BOTTOM
-                    {
-                        WriteAt("#", xCoord + x, yCoord + _height);
+                        WriteAt("#", xCoord + x, yCoord); //TOP 
+                        Thread.Sleep(1);
+                        if (y <= _height)
+                        {
+                            WriteAt("#", xCoord + _length, yCoord + x); //RIGHT
+                            WriteAt("#", xCoord, yCoord + x); //LEFT
+                            Thread.Sleep(1);
+                            y++;
+                        }
+                        WriteAt("#", xCoord + x, yCoord + _height); //BOTTOM
+                        Thread.Sleep(1);
                     }
                     for (int x = 0; x < _length; x++) //SEPARATOR AT LINE 6
                     {
                         WriteAt("#", xCoord + x, yCoord + 5);
+                        Thread.Sleep(1);
                     }
-                    WriteAt("TERMINATOR TERMINAL V1.0", 5, 2);
-                    WriteAt("You pick 'em, we kill'em!", 5, 3);
-                    WriteAt("LOGGED IN AS: " + Username, 5, 6);
+                    WriteAtJustified("TERMINATOR TERMINAL V1.0", 2);
+                    WriteAtJustified("You pick 'em, we kill'em!", 3);
+                    WriteAtJustified("LOGGED IN AS: " + Username, 6);
 
                     WriteAt("1. Create a Kill List", 4, 8);
                     WriteAt("2. Upload Kill List to current robot", 4, 9);
                     WriteAt("3. Activate/Deactivate current robot", 4, 10);
                     WriteAt("4. See details of current robot activity", 4, 11);
                     WriteAt("5. Self destruct robot **WARNING**", 4, 12);
-                    WriteAt("ESC: LOGOUT/BACK", 8, 14);
+                    WriteAtJustified("ESC: LOGOUT", 14);
                     break;
             }
         }
 
         static void WriteAt(string s, int x, int y)
         {
-            try
-            {
-                Console.SetCursorPosition(xCoord + x, yCoord + y);
-                Console.Write(s);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.Clear();
-                Console.WriteLine(e.Message);
-            }
+            Console.SetCursorPosition(xCoord + x, yCoord + y);
+            Console.Write(s);
+        }
+
+        static void WriteAtJustified(string s, int y)
+        {
+            int justifiedX = Convert.ToInt32(Math.Floor((double) (_length - s.Length) / 2));
+            Console.SetCursorPosition(xCoord + justifiedX, yCoord + y);
+            Console.Write(s);
         }
 
         static void ClearCurrentConsoleLine(int repX, int repY) //OVERLOADS TO MANUALLY REPOSITION THE CURSOR
@@ -236,6 +237,52 @@ namespace T800
             Console.SetCursorPosition(0, Console.CursorTop);
             Console.Write(new string(' ', Console.WindowWidth));
             Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        static void SideBox(int choice)
+        {
+            
+        }
+
+        static void UnderBox()
+        {
+            if (!DrawerOut)
+            {
+                for (int height = 0; height < 6; height++)
+                {
+                    //Emulate a sliding-box animation
+                    WriteAt("#", 0, _height + height);
+                    WriteAt("#", _length, _height + height);
+                    for (int x = 1; x < _length; x++) 
+                    {
+                        WriteAt("#", _length - x, _height + height);
+                        if (height != 5 && height != 0)
+                        {
+                            WriteAt(" ", _length - x, _height + height);
+                        }
+
+                    }
+                    Thread.Sleep(8);
+                }
+                DrawerOut = true;
+            } else
+            {
+                for (int height = 5; height > 0; height--)
+                {
+                    //Emulate a sliding-box animation
+                    WriteAt(" ", 0, _height + height);
+                    WriteAt(" ", _length, _height + height);
+                    if (height != 0)
+                    {
+                        for (int x = 0; x < _length + 1; x++)
+                        {
+                            WriteAt(" ", _length - x, _height + height);
+                        }
+                    }
+                    Thread.Sleep(8);
+                }
+                DrawerOut = false;
+            }
         }
 
         static void ClearInside()
